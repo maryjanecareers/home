4e2ad5d6c7101629a4335105780850e37531c7b3
Lisa Trevis <lisa.trevis@zaner-bloser.com>
2024-12-29 16:04:52 -0600

    Styled Welcome section, added images
    Gave section a background color.
    Gave text different colors.
    Broke p and img pairs into separate articles for flex styling purposes.
    Grabbed some images from google but we'll need to find free/open source images for going live (these are just placeholder images)
    Added another color (light purple) to the TW config file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
     screens: {
-      'phone': '480px',
+      phone: '480px',
       // => @media (min-width: 640px) { ... }
 
-      'tablet': '640px',
+      tablet: '640px',
       // => @media (min-width: 640px) { ... }
 
-      'laptop': '1024px',
+      laptop: '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1280px',
+      desktop: '1280px',
       // => @media (min-width: 1280px) { ... }
     },
     colors: {
-      'contrast': '#973ed9',
+      contrast: '#973ed9',
+      'contrast-lightest': '#F5ECFB',
       'primary-25': 'a5c697',
       'primary-75': '#63904f',
       'primary-lightest': '#e9f3e4',
@@ -24,27 +25,27 @@ module.exports = {
       'primary-2': '#bedbae',
       'primary-3': '#a8ce94',
       'primary-4': '#92c27b',
-      'primary': '#7bb661',
+      primary: '#7bb661',
       'primary-6': '#669551',
       'primary-7': '#527541',
       'primary-8': '#3e5732',
       'primary-9': '#2b3b24',
       'primary-darkest': '#192016',
-      'white': '#ffffff',
-      'black': '#000000',
+      white: '#ffffff',
+      black: '#000000',
     },
     fontFamily: {
       sans: ['Roboto', 'sans-serif'],
     },
     extend: {
       spacing: {
-        '128': '32rem',
-        '144': '36rem',
+        128: '32rem',
+        144: '36rem',
       },
       borderRadius: {
         '4xl': '2rem',
-      }
-    }
+      },
+    },
   },
   plugins: [],
 }
