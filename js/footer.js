1c1d4e4c4c94b53589fef8d2296b8841bb2355e2
Lisa Trevis <lisa.trevis@zaner-bloser.com>
2024-12-30 10:30:04 -0600

    Updated footer content/styling
    Reduced size of logo and added some padding/border radius
    Removed extra Items but kept structure in case we add more in the future
    Mirrored main nav with linked items
    Put all copyright info on one line to reduce height of footer

--- js/footer.js
+++ js/footer.js
@@ -1,36 +1,23 @@
-const footerTemplate = document.createElement("template");
-const year  = new Date().getFullYear();
+const footerTemplate = document.createElement('template')
+const year = new Date().getFullYear()
 
-footerTemplate.innerHTML=`
+footerTemplate.innerHTML = `
         
         <footer class="bg-primary-75 text-white">
-            <section class="grid grid-cols-4 gap-4 w-screen">
-                <img src="images/logo.png" alt="MaryJane Careers Logo">
-                <ul class="pt-8">
-                    <li class="pb-4">item 1</li>
-                    <li class="pb-4">item 2</li>
-                    <li class="pb-4">item 3</li>
-                    <li class="pb-4">item 4</li>
-                    <li class="pb-4">item 5</li>
+            <section class="grid grid-rows-1 grid-flow-col items-center gap-2 w-screen">
+                <img src="images/logo.png" alt="MaryJane Careers Logo" class="size-24 rounded-xl ml-8 my-4">
+                <ul class="pt-8 underline underline-offset-4">
+                    <li class="pb-4"><a href="/tester.html">Be A Tester</li>
                 </ul>
-                <ul class="pt-8">
-                    <li class="pb-4">item 1</li>
-                    <li class="pb-4">item 2</li>
-                    <li class="pb-4">item 3</li>
-                    <li class="pb-4">item 4</li>
-                    <li class="pb-4">item 5</li>
+                <ul class="pt-8 underline underline-offset-4">
+                    <li class="pb-4"><a href="/about.html">About</li>
                 </ul>
-                <ul class="pt-8">
-                    <li class="pb-4">item 1</li>
-                    <li class="pb-4">item 2</li>
-                    <li class="pb-4">item 3</li>
-                    <li class="pb-4">item 4</li>
-                    <li class="pb-4">item 5</li>
+                <ul class="pt-8 underline underline-offset-4">
+                    <li class="pb-4"><a href="/contact-us.html">Contact</li>
                 </ul>
             </section>
-            <p class="text-white w-full text-center">MaryJane Careers<br>&#169; 2023-<span id="year"></span></p>
-            <p class="text-white text-center pb-8">A Tech Budz LLC company</p>
+            <p class="text-white w-full text-center pb-8">MaryJane Careers &#169; 2023-<span id="year"></span> | A Tech Budz LLC company</p>
         </footer>
 `
-document.body.appendChild(footerTemplate.content);
-document.getElementById("year").innerHTML = year;
+document.body.appendChild(footerTemplate.content)
+document.getElementById('year').innerHTML = year
